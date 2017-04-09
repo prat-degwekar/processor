10cfe10c50a6e32eac41c533636aba03d92114e1
prat-degwekar <prat.degwekar@gmail.com>
2017-04-09 13:05:47 +0530

    updated alu with placeholders for all functions and file for all logical functions of alu

--- alu.v
+++ alu.v
@@ -1,7 +1,7 @@
 `include "dff.v"
 
 
-module decoder(opcode[4:0] , out[31:0] , enable);				//decoder module with 5 bit input and 32 bit output
+module decoder(opcode[4:0] , out[31:0] , enable);				//decoder module with 5 bit input and 32 bit output - non pipelined
 	input [4:0]opcode;
 	output [31:0]out;
 	input enable;
@@ -52,7 +52,7 @@ endmodule
 
 endmodule*/
 
-module tsg(in , state , out , clk);								//one bit tri state gate with input in , state and clock
+module tsg(in , state , out , clk);								//one bit tri state gate with input in , state and clock - non pipelined
 	input in , state , clk;										//leaving it non-pipelined
 	output out;
 
@@ -74,14 +74,14 @@ module tsg(in , state , out , clk);								//one bit tri state gate with input i
 
 endmodule // tsg
 
-module tsg_32(in , state , out , clk);							//32 bit tri state gate array accepting 32 bit input <in> , one bit state <state> , 32 bit output <out> and clock
+module tsg_32(in , state , out , clk);							//32 bit tri state gate array accepting 32 bit input <in> , one bit state <state> , 32 bit output <out> and clock - non pipelined
 
 	input [31:0]in;
 	input state , clk;
 	
 	output [31:0] out;
 
-	reg [31:0] out;
+	reg [31:0] out;												//add dff_32 after this line for pipeline purposes
 
 	always @(in or state or clk)								//assigns 32 bit output 
 	begin
@@ -227,3 +227,95 @@ module tsg32_tb();												//testbench for 32 bit tri state gate modifiable e
 	end // initial
 
 endmodule // tsg_tb
+
+module alu(opcode , a , b , enable , out , clk);				//alu that takes opcode and 2 inputs and performs functions - non pipelined
+
+	input [31:0] a , b;
+	input clk , enable;
+	input [4:0] opcode;
+
+	output [31:0] out;
+
+	wire [31:0] out , code;
+
+	decoder dec(opcode , code , enable);
+
+	wire [31:0] w1 , w2 , w3 , w4 , w5 , w6 , w7 , w8 , w9 , w10 , w11 , w12 , w13 , w14 , w15 , w16;
+	wire [31:0] s1 , s2 , s3 , s4 , s5 , s6 , s7 , s8 , s9 , s10 , s11 , s12 , s13 , s14 , s15 , s16;
+
+	tsg_32 t1_1(a , code[0] , w1 , clk);							//tsg for adder a
+	tsg_32 t1_2(b , code[0] , s1 , clk);							//tsg for adder b
+
+	tsg_32 t2_1(a , code[1] , w2 , clk);							//tsg for adder w/c a
+	tsg_32 t2_2(b , code[1] , s2 , clk);							//tsg for adder w/c b
+
+	tsg_32 t3_1(a , code[2] , w3 , clk);							//tsg for subtract a
+	tsg_32 t3_2(b , code[2] , s3 , clk);							//tsg for subtract b
+
+	tsg_32 t4_1(a , code[3] , w4 , clk);							//tsg for subtract w/b a
+	tsg_32 t4_2(b , code[3] , s4 , clk);							//tsg for subtract w/b b
+
+	tsg_32 t5_1(a , code[4] , w5 , clk);							//tsg for multiply a
+	tsg_32 t5_2(b , code[4] , s5 , clk);							//tsg for multiply b
+
+	tsg_32 t6_1(a , code[5] , w6 , clk);							//tsg for floating point adder a
+	tsg_32 t6_2(b , code[5] , s6 , clk);							//tsg for floating point adder b
+
+	tsg_32 t7_1(a , code[6] , w7 , clk);							//tsg for floating subtract a
+	tsg_32 t7_2(b , code[6] , s7 , clk);							//tsg for floating subtract b
+
+	tsg_32 t8_1(a , code[7] , w8 , clk);							//tsg for floating multiply a
+	tsg_32 t8_2(b , code[7] , s8 , clk);							//tsg for floating multiply b
+
+	tsg_32 t9_1(a , code[8] , w9 , clk);							//tsg for AND a
+	tsg_32 t9_2(b , code[8] , s9 , clk);							//tsg for AND b
+
+	tsg_32 t10_1(a , code[9] , w10 , clk);							//tsg for OR a
+	tsg_32 t10_2(b , code[9] , s10 , clk);							//tsg for OR b
+
+	tsg_32 t11_1(a , code[10] , w11 , clk);							//tsg for XOR a
+	tsg_32 t11_2(b , code[10] , s11 , clk);							//tsg for XOR b
+
+	tsg_32 t12_1(a , code[11] , w12 , clk);							//tsg for NAND a
+	tsg_32 t12_2(b , code[11] , s12 , clk);							//tsg for NAND b
+
+	tsg_32 t13_1(a , code[12] , w13 , clk);							//tsg for NOR a
+	tsg_32 t13_2(b , code[12] , s13 , clk);							//tsg for NOR b
+
+	tsg_32 t14_1(a , code[13] , w14 , clk);							//tsg for XNOR a
+	tsg_32 t14_2(b , code[13] , s14 , clk);							//tsg for XNOR b
+
+	//write overloaded alu with only 1 input
+
+	tsg_32 t15_1(a , code[14] , w15 , clk);							//tsg for NOT a
+	tsg_32 t15_2(b , code[14] , s15 , clk);							//tsg for NOT b
+
+	tsg_32 t16_1(a , code[15] , w16 , clk);							//tsg for NEGATION a
+	tsg_32 t16_2(b , code[15] , s16 , clk);							//tsg for NEGATION b
+
+	//other alu
+
+	//adder module(w1 , s1 , cin//=0 , out , cout , clk);			//addition
+	//adder module(w2 , s2 , cin//=1 , out , cout , clk);			//addition with carry
+
+	//adder module(w3 , not(s3) , cin//=1 , out , cout , clk);		//subtraction
+	//adder module(w4 , not(s4) , cin//=0 , out , cout , clk);		//subtraction with borrow a.k.a barrow
+
+	//multiplication module(w5 , s5 , out , cout , clk);			//multiplication
+
+	//float adder(w6 , s6 , cin , out , cout , clk);				//floating point adder
+	//float adder(w7 , not(s7) , cin// = 1 , out , cout , clk);		//floating point subtraction
+	//float multiply(w8 , s8 , out , cout , clk);					//floating point multiply
+
+	//And_32(w9 , s9 , out , clk);									//AND
+	//Or_32(w10 , s10 , out , clk);									//OR
+	//Xor_32(w11 , s11 , out , clk);								//XOR
+	//Nand_32(w12 , s12 , out , clk);								//Nand
+	//Nor_32(w13 , s13 , out , clk);								//Nor
+	//Xnor_32(w14 , s14 , out , clk);								//Xnor
+
+	//Not_32(w15 , out , clk);										//NOT
+
+	//Neg_32(w16 , out , clk);										//2's Complement - yet to write
+
+endmodule // alu
---
+++ logical_mods.v
@@ -0,0 +1,164 @@
+module And_32(a , b , out , clk);							//working non-pipelined
+
+	input [31:0] a , b;
+	input clk;
+
+	output [31:0] out;
+
+	wire [31:0] out;
+
+	assign out = a&b;
+
+endmodule // And_32
+
+module Or_32(a , b , out , clk);							//working non-pipelined
+
+	input [31:0] a , b;
+	input clk;
+
+	output [31:0] out;
+
+	wire [31:0] out;
+
+	assign out = a|b;
+
+endmodule // Or_32
+
+module Xor_32(a , b , out , clk);							//working non-pipelined
+
+	input [31:0] a , b;
+	input clk;
+
+	output [31:0] out;
+
+	wire [31:0] out;
+
+	assign out = a^b;
+
+endmodule // Xor_32
+
+module Nand_32(a , b , out , clk);							//working non-pipelined
+
+	input [31:0] a , b;
+	input clk;
+
+	output [31:0] out;
+
+	wire [31:0] out;
+	wire [31:0] temp;
+
+	assign temp = a&b;
+	assign out = ~temp;										//try assign out = a ~& b;
+
+endmodule // Nand_32
+
+module Nor_32(a , b , out , clk);							//working non-pipelined
+
+	input [31:0] a , b;
+	input clk;
+
+	output [31:0] out;
+
+	wire [31:0] out;
+	wire [31:0] temp;
+
+	assign temp = a|b;
+	assign out = ~temp;										//try assign out = a ~| b;
+
+endmodule // Nor_32
+
+module Xnor_32(a , b , out , clk);							//working non-pipelined
+
+	input [31:0] a , b;
+	input clk;
+
+	output [31:0] out;
+
+	wire [31:0] out;
+	wire [31:0] temp;
+
+	assign temp = a^b;
+	assign out = ~temp;										//try assign out = a ~^ b;
+
+endmodule // Xnor_32
+
+module Not_32(a , out , clk);								//working - non pipelined
+
+	input [31:0] a;
+	input clk;
+
+	output [31:0] out;
+
+	wire [31:0] out;
+
+	assign out = ~a;										//try !a if ~a does not work
+
+endmodule // Not_32
+
+module tb();
+
+	reg [31:0]a , b;
+	reg clk;
+
+	wire [31:0] out1 , out2 , out3 , out4 , out5 , out6 , out7 , out8;
+
+	And_32 And(a , b , out1 , clk);
+
+	Or_32 Or(a , b , out2 , clk);
+
+	Xor_32 Xor(a , b , out3 , clk);
+
+	Nand_32 Nand(a , b , out4 , clk);
+
+	Nor_32 Nor(a , b , out5 , clk);
+
+	Xnor_32 Xnor(a , b , out6 , clk);
+
+	Not_32 Na(a , out7 , clk);
+
+	Not_32 Nb(b , out8 , clk);
+
+	initial begin
+
+		clk = 1'b0;
+
+	end
+
+	initial begin
+
+		#2
+
+		a = 32'b00110011001100110011001100110011;
+		b = 32'b11001100110011001100110011001100;
+
+		#2
+
+		a = 32'b11111111111111111111111111111111;
+		b = 32'b11110000111100001111000011110000;
+
+		#2
+
+		a = 32'b01010101010101010101010101010101;
+		b = 32'b10101010101010101010101010101010;
+
+		#2
+
+		a = 32'b00000000000000000000000000000000;
+		b = 32'b11111111000000001111111100000000;
+
+	end
+
+	initial begin
+
+		$monitor($time,"  a = %b , b = %b \n and = %b\nor = %b\nxor = %b\nnand = %b\nnor = %b\nxnor = %b\nnot a = %b\nnot b = %b\n\n" , a , b , out1 , out2 , out3 , out4 , out5 , out6 , out7 , out8);
+		/*$monitor($time,"  a = %b , b = %b , out2 = %b\n" , a , b , out2);
+		$monitor($time,"  a = %b , b = %b , out3 = %b\n" , a , b , out3);
+		$monitor($time,"  a = %b , b = %b , out4 = %b\n" , a , b , out4);
+		$monitor($time,"  a = %b , b = %b , out5 = %b\n" , a , b , out5);
+		$monitor($time,"  a = %b , b = %b , out6 = %b\n" , a , b , out6);
+		$monitor($time,"  a = %b , b = %b , out7 = %b\n" , a , b , out7);
+		$monitor($time,"  a = %b , b = %b , out8 = %b\n" , a , b , out8);*/
+
+	end
+
+endmodule
