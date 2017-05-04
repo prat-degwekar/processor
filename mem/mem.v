f1ad5e8f374b073022dc55d4a52db7abe8234231
reisub0 <reisub0@gmail.com>
2017-05-04 17:11:38 +0530

    fin?

--- mem/mem.v
+++ mem/mem.v
@@ -6,7 +6,7 @@ input read,write;
 output [31:0] out;
 
 reg [31:0]  out;
-reg [8192:0] mem [31:0];
+reg [31:0] mem [8192:0];
 
 always @(*) begin
 	if (read)
@@ -16,6 +16,15 @@ always @(*) begin
 end
 
 initial begin
+	//Data memory
+	mem[0]=32'b00000000000000000000000000000000; // binary value of 0 in fp
+	mem[1]=32'b00111111100000000000000000000000; // binary value of 1 in fp
+	mem[2]=32'b01000000111000000000000000000000; // binary value of 7 in fp
+	//Program memory
+	mem[100]=32'b10000_00000_0000000000000000000000; // R0 <- mem[0]
+	mem[101]=32'b10000_00001_0000000000000000000001; // R1 <- mem[1]
+	mem[102]=32'b10000_00001_0000000000000000000001; // R1 <- mem[1]
+
 	/* insert program code here */
 end
 
